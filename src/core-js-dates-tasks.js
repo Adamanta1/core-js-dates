ba2e89b5059989ba93a280c1792a48081f011099
adamanta1 <elena.brashkina2020@gmail.com>
2024-03-20 00:07:15 +0400

    feat: solve 4 tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -17,8 +17,9 @@
  * '01 Jan 1970 00:00:00 UTC' => 0
  * '04 Dec 1995 00:12:00 UTC' => 818035920000
  */
-function dateToTimestamp(/* date */) {
-  throw new Error('Not implemented');
+function dateToTimestamp(date) {
+  const dateObj = new Date(date);
+  return dateObj.getTime();
 }
 
 /**
@@ -31,8 +32,13 @@ function dateToTimestamp(/* date */) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const hours = date.getHours() > 9 ? date.getHours() : `0${date.getHours()}`;
+  const minutes =
+    date.getMinutes() > 9 ? date.getMinutes() : `0${date.getMinutes()}`;
+  const seconds =
+    date.getSeconds() > 9 ? date.getSeconds() : `0${date.getSeconds()}`;
+  return `${hours}:${minutes}:${seconds}`;
 }
 
 /**
@@ -46,8 +52,18 @@ function getTime(/* date */) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const dateObj = new Date(date);
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  return days[dateObj.getUTCDay()];
 }
 
 /**
@@ -61,8 +77,20 @@ function getDayName(/* date */) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const weekDay = date.getDay();
+  let monthDay = date.getDate();
+  for (let i = 0; i < 7; i += 1) {
+    if (weekDay === i && i < 5) {
+      monthDay += 5 - i;
+    } else if (weekDay === i && i === 5) {
+      monthDay += i + 2;
+    } else if (weekDay === i && i === 6) {
+      monthDay += i;
+    }
+  }
+  date.setDate(monthDay);
+  return date;
 }
 
 /**
